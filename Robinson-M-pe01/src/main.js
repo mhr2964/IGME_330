892c042e592cf5128517f1c9a4f90db9728ced7d
mhr2964 <mhr2964@rit.edu>
2023-09-04 23:35:15 -0400

    PE01

---
+++ Robinson-M-pe01/src/main.js
@@ -0,0 +1,31 @@
+"use strict";
+
+window.onload = function() {
+    const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
+
+    const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
+
+    const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
+
+    //console.log(words1[0]);
+
+    //Getting the DOM Elements and setting the button's onclick
+    const babbleButton = document.querySelector("#myButton");
+    babbleButton.onclick = generateTechnobabble;
+
+    const output = document.querySelector("#output");
+
+    //Displaying babble when the page loads
+    generateTechnobabble();
+
+    //Functions to generate the babble and get a random index from an array
+    function generateTechnobabble() {
+
+        function getRandomIndex(array) {
+            let random = Math.floor(Math.random() * (array.length));
+            return array[random];
+        }
+
+        output.textContent = `${getRandomIndex(words1)} ${getRandomIndex(words2)} ${getRandomIndex(words3)}`;
+    }
+}
