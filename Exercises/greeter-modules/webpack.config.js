b7d17cb31cd435f3f95c612478cf3e9ab7545ca3
mhr2964 <mhr2964@rit.edu>
2023-10-30 00:17:32 -0400

    whatever

---
+++ Exercises/greeter-modules/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    mode: 'production',
+    entry: ['./src/main.js'],
+    output: {
+        filename: './bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                },
+            },
+        }, ],
+    }
+};
